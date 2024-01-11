5a9a9bd8a7d711d33192c4dbbeb4ba1c1b533f39
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2024-01-11 16:54:17 +0530

    Add files via upload

---
+++ PRO-C215-StudentBoilerplate-main/213s/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World");
+});
+server.listen(3030);
